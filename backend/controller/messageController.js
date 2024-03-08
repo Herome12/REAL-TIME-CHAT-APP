68f36b5ad9bdb29379e4dd9b0e5fd0b87c609dbd
Ujjwal Pal <palujjwal1112@gmail.com>
2024-03-08 18:36:33 +0530

    .

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -3,5 +3,22 @@ const catchAsynError = require("../middleware/catchAsynError")
 const errorhandler = require("../utils/errorhandler")
 const sendToken = require("../utils/sendToken")
 const conversation = require("../schema/conversation")
+const conversation = require("../schema/conversation")
 
+
+exports.sendMessage = catchAsynError(async(req,res,next)=>{
+    const {message} = req.body;
+    const {id:reciverID} = req.params
+    const senderID = req.user._id
+
+    let Conversation = await conversation.findOne({
+        pariticipants:{$all:[senderID,reciverID]}
+    })
+
+    if(!Conversation){
+        Conversation = await conversation.create({
+            participants:[senderID,reciverID]
+        })
+    }
+})
 
